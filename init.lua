b51d903b14a274c556e8e8ef54237136dc0f4551
Jordan Irwin <antumdeluge@gmail.com>
2021-06-14 12:50:23 -0700

    Fix glass sound name

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ end
 function sounds.node_glass(tbl)
 	tbl = tbl or {}
 	tbl.footstep = tbl.footstep or {name="sounds_glass_footstep", gain=0.3}
-	tbl.dig = tbl.dig or {name="default_glass_footstep", gain=0.5}
+	tbl.dig = tbl.dig or {name="sounds_glass_footstep", gain=0.5}
 	tbl.dug = tbl.dug or {name="sounds_glass_break", gain=1.0}
 	sounds.node(tbl)
 
