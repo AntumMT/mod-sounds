b87a7d7b0979f1a6f1cafba190f65d1a1b096151
Jordan Irwin <antumdeluge@gmail.com>
2021-06-14 15:34:52 -0700

    Add functions for dirt, gravel, ice, leaves, metal, sand, snow, water, & wood node sounds

--- init.lua
+++ init.lua
@@ -12,6 +12,17 @@ function sounds.node(tbl)
 	return tbl
 end
 
+function sounds.node_dirt(tbl)
+	tbl = tbl or {}
+
+	tbl.footstep = tbl.footstep or {name="sounds_step_dirt", gain=0.4}
+	tbl.dug = tbl.dug or {name="sounds_step_dirt", gain=1.0}
+	tbl.place = tbl.place or {name="sounds_place_node_soft", gain=1.0}
+
+	sounds.node(tbl)
+	return tbl
+end
+
 function sounds.node_glass(tbl)
 	tbl = tbl or {}
 
@@ -23,11 +34,99 @@ function sounds.node_glass(tbl)
 	return tbl
 end
 
+function sounds.node_gravel(tbl)
+	tbl = tbl or {}
+
+	tbl.footstep = tbl.footstep or {name="sounds_step_gravel", gain=0.1}
+	tbl.dig = tbl.dig or {name="sounds_dig_gravel", gain=0.35}
+	tbl.dug = tbl.dug or {name="sounds_dug_gravel", gain=1.0}
+	tbl.place = tbl.place or {name="sounds_place_node_soft", gain=1.0}
+
+	sounds.node(tbl)
+	return tbl
+end
+
+function sounds.node_ice(tbl)
+	tbl = tbl or {}
+
+	tbl.footstep = tbl.footstep or {name="sounds_step_ice", gain=0.3}
+	tbl.dig = tbl.dig or {name="sounds_dig_ice", gain=0.5}
+	tbl.dug = tbl.dug or {name="sounds_dug_ice", gain=0.5}
+
+	sounds.node(tbl)
+	return tbl
+end
+
+function sounds.node_leaves(tbl)
+	tbl = tbl or {}
+
+	tbl.footstep = tbl.footstep or {name="sounds_step_grass", gain=0.45}
+	tbl.dug = tbl.dug or {name="sounds_step_grass", gain=0.7}
+	tbl.place = tbl.place or {name="sounds_place_node_soft", gain=1.0}
+
+	sounds.node(tbl)
+	return tbl
+end
+
+function sounds.node_metal(tbl)
+	tbl = tbl or {}
+
+	tbl.footstep = tbl.footstep or {name="sounds_step_metal", gain=0.4}
+	tbl.dig = tbl.dig or {name="sounds_dig_metal", gain=0.5}
+	tbl.dug = tbl.dug or {name="sounds_dug_metal", gain=0.5}
+	tbl.place = tbl.place or {name="sounds_place_metal", gain=0.5}
+
+	sounds.node(tbl)
+	return tbl
+end
+
+function sounds.node_sand(tbl)
+	tbl = tbl or {}
+
+	tbl.footstep = tbl.footstep or {name="sounds_step_sand", gain=0.05}
+	tbl.dug = tbl.dug or {name="sounds_step_sand", gain=0.15}
+	tbl.place = tbl.place or {name="sounds_place_node_soft", gain=1.0}
+
+	sounds.node(tbl)
+	return tbl
+end
+
 function sounds.node_stone(tbl)
 	tbl = tbl or {}
 
 	tbl.footstep = tbl.footstep or {name="sounds_step_hard", gain=0.3}
 	tbl.dug = tbl.dug or {name="sounds_step_hard", gain=1.0}
+
+	sounds.node(tbl)
+	return tbl
+end
+
+function sounds.node_snow(tbl)
+	tbl = tbl or {}
+
+	tbl.footstep = tbl.footstep or {name="sounds_step_snow", gain=0.2}
+	tbl.dig = tbl.dig or {name="sounds_step_snow", gain=0.3}
+	tbl.dug = tbl.dug or {name="sounds_step_snow", gain=0.3}
+	tbl.place = tbl.place or {name="sounds_place_node_soft", gain=1.0}
+
+	sounds.node(tbl)
+	return tbl
+end
+
+function sounds.node_water(tbl)
+	tbl = tbl or {}
+
+	tbl.footstep = tbl.footstep or {name="sounds_step_water", gain=0.2}
+
+	sounds.node(tbl)
+	return tbl
+end
+
+function sounds.node_wood(tbl)
+	tbl = tbl or {}
+
+	tbl.footstep = tbl.footstep or {name="sounds_step_wood", gain=0.3}
+	tbl.dug = tbl.dug or {name="sounds_step_wood", gain=1.0}
 
 	sounds.node(tbl)
 	return tbl
