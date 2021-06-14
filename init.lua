79bfcadde0400d94f104553f1e868c42c0ddfcb6
Jordan Irwin <antumdeluge@gmail.com>
2021-06-14 14:21:02 -0700

    Rename "foostep" sounds to "step"

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ sounds = {}
 
 function sounds.node(tbl)
 	tbl = tbl or {}
+
 	tbl.footstep = tbl.footstep or {name="", gain=1.0}
 	tbl.dug = tbl.dug or {name="sounds_dug_node", gain=0.25}
 	tbl.place = tbl.place or {name="sounds_place_node", gain=1.0}
@@ -11,21 +12,23 @@ function sounds.node(tbl)
 	return tbl
 end
 
-function sounds.node_stone(tbl)
+function sounds.node_glass(tbl)
 	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or {name="sounds_footstep_hard", gain=0.3}
-	tbl.dug = tbl.dug or {name="sounds_footstep_hard", gain=1.0}
-	sounds.node(tbl)
 
+	tbl.footstep = tbl.footstep or {name="sounds_step_glass", gain=0.3}
+	tbl.dig = tbl.dig or {name="sounds_step_glass", gain=0.5}
+	tbl.dug = tbl.dug or {name="sounds_break_glass", gain=1.0}
+
+	sounds.node(tbl)
 	return tbl
 end
 
-function sounds.node_glass(tbl)
+function sounds.node_stone(tbl)
 	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or {name="sounds_footstep_glass", gain=0.3}
-	tbl.dig = tbl.dig or {name="sounds_footstep_glass", gain=0.5}
-	tbl.dug = tbl.dug or {name="sounds_break_glass", gain=1.0}
-	sounds.node(tbl)
 
+	tbl.footstep = tbl.footstep or {name="sounds_step_hard", gain=0.3}
+	tbl.dug = tbl.dug or {name="sounds_step_hard", gain=1.0}
+
+	sounds.node(tbl)
 	return tbl
 end
